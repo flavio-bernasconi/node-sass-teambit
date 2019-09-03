744e17d6a0dbeea173535d21adabeac5eef98f12
flavio-bernasconi <info@flaviobernasconi.it>
2019-09-03 18:48:09 +0200

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-// alert("Hello World")
+$ = require('jquery');
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+
+    if (scroll >= 500) {
+        //clearHeader, not clearheader - caps H
+        $("header").addClass("fix");
+        $("nav").css("border-bottom" , "none")
+    }
+    else {
+      $("header").removeClass("fix");
+      $("nav").css("border-bottom" , "solid 1px rgb(144, 144, 144)")
+    }
+});
